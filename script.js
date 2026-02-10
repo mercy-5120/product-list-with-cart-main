2dd427a2ff83d1aaae793134a687295aa0ca4001
mercy-5120 <mgashau18@gmail.com>
2026-02-10 09:57:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,128 @@
+/*fetch('data.json')//used to request data from a server or a local file
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+});
+*/
+
+const products = [
+  {
+    image: {
+      thumbnail: "./assets/images/image-waffle-thumbnail.jpg",
+      mobile: "./assets/images/image-waffle-mobile.jpg",
+      tablet: "./assets/images/image-waffle-tablet.jpg",
+      desktop: "./assets/images/image-waffle-desktop.jpg",
+    },
+    name: "Waffle with Berries",
+    category: "Waffle",
+    price: 6.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-creme-brulee-thumbnail.jpg",
+      mobile: "./assets/images/image-creme-brulee-mobile.jpg",
+      tablet: "./assets/images/image-creme-brulee-tablet.jpg",
+      desktop: "./assets/images/image-creme-brulee-desktop.jpg",
+    },
+    name: "Vanilla Bean Crème Brûlée",
+    category: "Crème Brûlée",
+    price: 7.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-macaron-thumbnail.jpg",
+      mobile: "./assets/images/image-macaron-mobile.jpg",
+      tablet: "./assets/images/image-macaron-tablet.jpg",
+      desktop: "./assets/images/image-macaron-desktop.jpg",
+    },
+    name: "Macaron Mix of Five",
+    category: "Macaron",
+    price: 8.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-tiramisu-thumbnail.jpg",
+      mobile: "./assets/images/image-tiramisu-mobile.jpg",
+      tablet: "./assets/images/image-tiramisu-tablet.jpg",
+      desktop: "./assets/images/image-tiramisu-desktop.jpg",
+    },
+    name: "Classic Tiramisu",
+    category: "Tiramisu",
+    price: 5.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-baklava-thumbnail.jpg",
+      mobile: "./assets/images/image-baklava-mobile.jpg",
+      tablet: "./assets/images/image-baklava-tablet.jpg",
+      desktop: "./assets/images/image-baklava-desktop.jpg",
+    },
+    name: "Pistachio Baklava",
+    category: "Baklava",
+    price: 4.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-meringue-thumbnail.jpg",
+      mobile: "./assets/images/image-meringue-mobile.jpg",
+      tablet: "./assets/images/image-meringue-tablet.jpg",
+      desktop: "./assets/images/image-meringue-desktop.jpg",
+    },
+    name: "Lemon Meringue Pie",
+    category: "Pie",
+    price: 5.0,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-cake-thumbnail.jpg",
+      mobile: "./assets/images/image-cake-mobile.jpg",
+      tablet: "./assets/images/image-cake-tablet.jpg",
+      desktop: "./assets/images/image-cake-desktop.jpg",
+    },
+    name: "Red Velvet Cake",
+    category: "Cake",
+    price: 4.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-brownie-thumbnail.jpg",
+      mobile: "./assets/images/image-brownie-mobile.jpg",
+      tablet: "./assets/images/image-brownie-tablet.jpg",
+      desktop: "./assets/images/image-brownie-desktop.jpg",
+    },
+    name: "Salted Caramel Brownie",
+    category: "Brownie",
+    price: 4.5,
+  },
+  {
+    image: {
+      thumbnail: "./assets/images/image-panna-cotta-thumbnail.jpg",
+      mobile: "./assets/images/image-panna-cotta-mobile.jpg",
+      tablet: "./assets/images/image-panna-cotta-tablet.jpg",
+      desktop: "./assets/images/image-panna-cotta-desktop.jpg",
+    },
+    name: "Vanilla Panna Cotta",
+    category: "Panna Cotta",
+    price: 6.5,
+  },
+];
+
+const dessertsContainer = document.querySelector('#desserts-list');
+
+for(let i = 0; i < products.length; i++){
+    const product = products[i];
+    const productElement = document.createElement('li');
+    productElement.classList.add('dessert-item');
+    //productElement.setAttribute("class", "dessert-item");///Alternative way to set class
+    productElement.innerHTML = `
+    <picture>
+      <source media="(max-width: 768px)" srcset="${product.image.mobile}">
+      <source media="(max-width: 1024px)" srcset="${product.image.tablet}">
+      <img src="${product.image.desktop}" alt="${product.name}">
+    </picture>
+    <button class="add-to-cart-btn">Add to Cart</button>
+    <h3>${product.name}</h3>
+    <p>$${product.price.toFixed(2)}</p>
+    `;
+    dessertsContainer.appendChild(productElement);
+  }
